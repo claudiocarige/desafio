973fe388020394acb1f904abdd10444b4b6597ea
Claudio Pimentel <ccarige@gmail.com>
2026-08-29 11:33:36 -0300

    [ DESAFIO-001 ] feat: Adicionar SecurityConfig com autenticação básica e desabilitar CSRF

---
+++ src/main/java/br/com/claudiocarige/desafio/configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.claudiocarige.desafio.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(basic -> {
+                });
+
+        return http.build();
+    }
+}
